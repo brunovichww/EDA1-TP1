c011d3a43be5f2392aeca8a1e6970c9022884550
brunovichww <bruno.w.163@gmail.com>
2025-04-02 18:25:52 -0300

    guarda que el 3 esta medio rari

--- 3_tp1.c
+++ 3_tp1.c
@@ -6,7 +6,8 @@ Generar un algoritmo recursivo que calcule el k-ésimo número de la serie de Fi
 		terminoSeridFibonacci (5) => 8
  * ***/
  
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int terminoSeridFibonacci(int k){
 	//caso base
@@ -16,8 +17,78 @@ int terminoSeridFibonacci(int k){
 	return terminoSeridFibonacci(k-1)+ terminoSeridFibonacci(k-2);
 	}
 	
-int main(){
-	int kt = terminoSeridFibonacci(8);
-	printf("%d",kt);
-	}
+
+int main() {
+    char str[10];
+    printf("Ingrese un número: ");
+    fgets(str, sizeof(str), stdin);
+    
+    char *endptr;
+    long num = strtol(str, &endptr, 10);
+	
+    if (*endptr != '\0') {
+        printf("Error: No se pudo convertir completamente. Parte inválida: %s\n", endptr);
+    } else {
+        printf("Número convertido: %ld\n", num);
+    }
+
+    return 0;
+}	
+	
+	
+//int main(){
+	
+	//int kt = terminoSeridFibonacci(8);
+	//int a = 0;
+	//char str[10];
+	//char *endptr;
+    //int num;
+    
+	//while(a==0){
+	//printf("Ingrese un número: ");
+    //fgets(str, sizeof(str), stdin);
+	//num = strtol(str, &endptr, 10);
+	
+	//if (*endptr != '\0'){
+		//printf("debe ingresar un valor numérico.\n");
+		//}
+	//}
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main() {
+    char str[10];
+    char *endptr;
+    int num;
+    
+    while (1) {
+        printf("Ingrese un número: ");
+        fgets(str, sizeof(str), stdin);
+        num = strtol(str, &endptr, 10);
+
+        // Verificar si no se pudo convertir nada
+        if (str == endptr) {
+            printf("Debe ingresar un valor numérico.\n");
+            continue;
+        }
+
+        // Verificar que después del número solo haya espacios o el fin de la línea
+        while (*endptr != '\0' && *endptr != '\n') {
+            if (!isspace((unsigned char)*endptr)) {
+                printf("Debe ingresar solo números sin caracteres adicionales.\n");
+                break;
+            }
+            endptr++;
+        }
+
+        // Si llegó aquí, la entrada es válida
+        printf("Número ingresado: %d\n", num);
+        break;
+    }
+
+    return 0;
+}
 
