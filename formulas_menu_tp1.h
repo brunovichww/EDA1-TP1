bd439d47f67c4c693bb1fa2307aed4da06eb1579
Martin Campos <martinjcampos@yahoo.com.ar>
2025-04-08 22:24:42 -0300

    Create formulas_menu_tp1.h
    .h donde se colocan las firmas de las formulas para utilizar en el programa principal (main.c) llamando a cada ejercicio

---
+++ formulas_menu_tp1.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+void ejecutar_ejercicio_1();
+void ejecutar_ejercicio_2();
+void ejecutar_ejercicio_3();
+void ejecutar_ejercicio_4();
+void ejecutar_ejercicio_5();
