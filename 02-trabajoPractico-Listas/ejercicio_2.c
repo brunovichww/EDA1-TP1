e4bb101ee8b18f8c3fbf25c664a90ebbc620f743
brunovichww <bruno.w.163@gmail.com>
2025-05-03 20:20:17 -0300

    Agregando .h y .c dados por el equipo docente

---
+++ 02-trabajoPractico-Listas/ejercicio_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "tp_2_listas.h"
+
+// Punto 2 a y b
+Lista verElementosQueNoSeRepiten(Lista l1, Lista l2);
+// Punto 2 C
+Lista verElementosRepetidos(Lista l1, Lista l2);
